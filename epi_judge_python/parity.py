ef429b67cc061fd65da17b7a333a1095b95d8f0e
Denys Lins <denyslins@gmail.com>
2020-01-20 11:33:51 -0300

    Add better algorithm for parity

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,12 +1,22 @@
 from test_framework import generic_test
 
+# V1
+# def parity(x: int) -> int:
+#     # TODO - you fill in here.
+#     result = 0
+#     while x:
+#         result ^= x & 1
+#         x >>= 1
+#     return result
+
 
 def parity(x: int) -> int:
+    # V2
     # TODO - you fill in here.
     result = 0
     while x:
-        result ^= x & 1
-        x >>= 1
+        result ^= 1
+        x &= x - 1
     return result
 
 
