f9d68d6d2934f11e5a2addcd43c559770e0a5c1c
Denys Lins <denyslins@gmail.com>
2020-02-04 18:51:03 -0300

    Add reverse_digits - WIP

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -3,7 +3,15 @@ from test_framework import generic_test
 
 def reverse(x: int) -> int:
     # TODO - you fill in here.
-    return 0
+    result, remaining = 0, abs(x)
+    print(result, remaining)
+    while remaining:
+        print(result * 10)
+        print(remaining % 10)
+        result = result * 10 + remaining % 10
+        print result
+        remaining = remaining // 10
+    return -result if x < 0 else result
 
 
 if __name__ == '__main__':
