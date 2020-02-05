0c9ab23ac47286591ada4854988dca0f8f336373
Denys Lins <denyslins@gmail.com>
2020-02-05 00:11:55 -0300

    WIP - is_number_palindromic

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,30 @@
+import math
 from test_framework import generic_test
 
 
 def is_palindrome_number(x: int) -> bool:
     # TODO - you fill in here.
+    if x < 0:
+        return False
+
+    if x == 0:
+        return True
+
+    n = float('inf')
+    while n > 2 and x != 0:
+        print('')
+        print(x, x % 10)
+        lsd = x % 10
+        print('lsd', lsd)
+        n = math.floor(math.log10(x)) + 1
+        print('n', n)
+        msd = int(x // math.pow(10, n-1))
+        print('msd', msd)
+        if (lsd != msd):
+            return False
+        else:
+            x = int(x % (msd * math.pow(10, n-1)))
+            x = x // 10
     return True
 
 
